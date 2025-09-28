6afdae7dc85d4c13fa0d295b467fc79e53bc692f
Kate Zavadskaya <ekm.zavadskaya@gmail.com>
2025-09-28 23:21:11 +0300

    Feat(hm3): added done hm3
    Add done hm3 with 4 files
    
    Docs: task_1.py - task_4.py

---
+++ homework/ekaterina_zavadskaya/Homework_3/task_2_division.py
@@ -0,0 +1,8 @@
+"""Даны числа x и y. Получить x − y / 1 + xy"""
+
+
+FIRST_VALUE = 3
+SECOND_VALUE = 5
+
+print(f'total is {(FIRST_VALUE - SECOND_VALUE) /
+                  1 + FIRST_VALUE * SECOND_VALUE}')
