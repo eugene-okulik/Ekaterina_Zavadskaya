4058de617405742846f462cb9c600d176b851d57
Kate Zavadskaya <ekm.zavadskaya@gmail.com>
2025-10-02 22:39:54 +0300

    Feat(fix): fixed flake8 with E126
    Fixed flake8 with E126 in MY_DICT
    
    Docs: task_all.py

--- homework/ekaterina_zavadskaya/Homework_4/task_all.py
+++ homework/ekaterina_zavadskaya/Homework_4/task_all.py
@@ -34,7 +34,7 @@ MY_DICT = {"my_tuple": (1, 2.2, "Hello", (6, 7, "mouse"), [4, 5.5, "dog"]),
                "hair": "blond"
            },
            "my_set": {80, "sun", -90, 100, "cloud"}
-           }
+}
 # 1
 print("1)", MY_DICT["my_tuple"][-1])
 
